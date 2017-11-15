7d860706fccf0804707487ac60cb0d7b6c0eafc7
Chaosruler <trunks_ishter@hotmail.com>
2017-11-15 03:57:49 +0200

    Added tests for remote

--- src/app/src/androidTest/java/com/example/chaosruler/msa_manager/remote_connection_test.kt
+++ src/app/src/androidTest/java/com/example/chaosruler/msa_manager/remote_connection_test.kt
@@ -16,40 +16,117 @@ class remote_connection_test {
     @Test
     fun remote_connection_test()
     {
+        val can_run_test = true
+
+        /*
+        if(!can_run_test)
+            return
+        */
+
         val con = InstrumentationRegistry.getTargetContext()
         var test_num = 1
-        val amount_of_tests = 1
+        val amount_of_tests = 7
         val amount_str = " Out of $amount_of_tests "
-        var vector:Vector<HashMap<String,String>> = Vector()
         val username = "chaosruler"
         val password = "*niu9p2w"
+        val db_name = "main"
+        val table_name = "projects"
+
+        val project_ID = "project_ID"
+        val project_name = "project_name"
+        val project_manager_name = "project_manager_name"
+
 
         System.out.println("This test requires internet connection!\ninitating ")
-        var remote_connection:remote_SQL_Helper
+        var remote_connection:remote_SQL_Helper? = null
         try
         {
             remote_connection = remote_SQL_Helper(con, username,password)
             Assert.assertTrue("remote database tests: connect #$test_num$amount_str: fails\n ", remote_connection.isValid() )
-            vector = remote_connection.get_all_table("main","projects")!!
-            var str:String = "DB length = ${vector.size}: "
-            for (item in vector)
-            {
-                for(value in item)
-                {
-                    str+="[${value.key}] = ${value.value} "
-                }
-                str+="\n"
-            }
-            Log.d("key",str)
-
-
-
             Log.d("remote database tests", "connect #$test_num$amount_str: success\n ")
         } catch (e: Exception) {
             Log.d("remote database tests", "connect #$test_num$amount_str: fails\n ")
         }
 
+        test_num++
 
+        try
+        {
+            var string_vector:Vector<String> = Vector()
+            string_vector.addElement(project_ID)
+            string_vector.addElement(project_name)
+            string_vector.addElement(project_manager_name)
+            var map:HashMap<String,String> = HashMap()
+            map[project_ID] = 1.toString()
+            map[project_name] = "'hi'"
+            map[project_manager_name] = "'bye'"
+            Assert.assertTrue("remote database tests: add #$test_num$amount_str: fails\n ", remote_connection!!.add_data(db_name,table_name,string_vector,map) )
+            Log.d("remote database tests", "add #$test_num$amount_str: success\n ")
+        } catch (e: Exception)
+        {
+            Log.d("remote database tests", "add #$test_num$amount_str: fails\n ")
+        }
+
+        test_num++
+
+        try
+        {
+
+            Assert.assertTrue("remote database tests: print all #$test_num$amount_str: fails\n ", remote_connection!!.get_all_table(db_name,table_name)!!.size==1 )
+            Log.d("remote database tests", "print all #$test_num$amount_str: success\n ")
+        } catch (e: Exception) {
+            Log.d("remote database tests", "print all#$test_num$amount_str: fails\n ")
+        }
+
+
+        test_num++
+
+        try
+        {
+            var map:HashMap<String,String> = HashMap()
+            map[project_ID] = 2.toString()
+            Assert.assertTrue("remote database tests: update #$test_num$amount_str: fails\n ", remote_connection!!.update_query(db_name,table_name,project_ID, arrayOf("1"),"int",map) )
+            Log.d("remote database tests", "update #$test_num$amount_str: success\n ")
+        } catch (e: Exception)
+        {
+            Log.d("remote database tests", "update #$test_num$amount_str: fails\n ")
+        }
+
+
+        test_num++
+
+        try
+        {
+
+            Assert.assertTrue("remote database tests: print all #$test_num$amount_str: fails\n ", remote_connection!!.get_all_table(db_name,table_name)!!.size==1 )
+            Log.d("remote database tests", "print all #$test_num$amount_str: success\n ")
+        } catch (e: Exception)
+        {
+            Log.d("remote database tests", "print all#$test_num$amount_str: fails\n ")
+        }
+
+
+        test_num++
+
+        try
+        {
+
+            Assert.assertTrue("remote database tests: remove #$test_num$amount_str: fails\n ", remote_connection!!.remove_data(db_name,table_name,project_manager_name, arrayOf("'bye'"),"varchar(max)") )
+            Log.d("remote database tests", "remove  #$test_num$amount_str: success\n ")
+        } catch (e: Exception) {
+            Log.d("remote database tests", "remove  #$test_num$amount_str: fails\n ")
+        }
+
+        test_num++
+
+        try
+        {
+            Assert.assertTrue("remote database tests: print all #$test_num$amount_str: fails\n ", remote_connection!!.get_all_table(db_name,table_name)!!.size==0 )
+            Log.d("remote database tests", "print all #$test_num$amount_str: success\n ")
+        } catch (e: Exception)
+        {
+            Log.d("remote database tests", "print all#$test_num$amount_str: fails\n ")
+        }
 
     }
 
