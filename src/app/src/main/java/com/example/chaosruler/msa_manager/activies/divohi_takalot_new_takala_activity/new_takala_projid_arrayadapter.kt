1d060df293a2f4819588102be4db5b588e3fd267
Zeev Melumian <trunks_ishter@hotmail.com>
2018-05-20 16:33:03 +0300

    Added jdoc

--- src/app/src/main/java/com/example/chaosruler/msa_manager/activies/divohi_takalot_new_takala_activity/new_takala_projid_arrayadapter.kt
+++ src/app/src/main/java/com/example/chaosruler/msa_manager/activies/divohi_takalot_new_takala_activity/new_takala_projid_arrayadapter.kt
@@ -10,9 +10,20 @@ import com.example.chaosruler.msa_manager.R
 import com.example.chaosruler.msa_manager.object_types.project_data
 import org.jetbrains.anko.textColor
 
+/**
+ * Array Adapter that gives projname by its projid
+ * @author Chaosruler972
+ */
 class new_takala_projid_arrayadapter(context: Context, resourceID: Int, list:List<project_data>):
         ArrayAdapter<project_data>(context, resourceID, list.toTypedArray())
 {
+    /**
+     * Creates a view that gives projname and black colored text style
+     * @author Chaosruler972
+     * @param position item position to generate
+     * @param convertView an instance of the view to generate
+     * @param parent the master view, meaning the listview, holding it
+     */
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val txtview = TextView(context)
         txtview.text = getItem(position).get_project_name() ?: ""
